3745a173ce35656938c397ba9eae8d4434578f13
Jonas Cahenzli <jonas.cahenzli@ost.ch>
2022-02-02 21:13:05 +0100

    SpielstandsManagerTest: JUnit test created for every method

--- src/test/java/SpielstandsManagerTest.java
+++ src/test/java/SpielstandsManagerTest.java
@@ -16,8 +16,9 @@ import main.java.SpielstandsManager;
  *
  */
 class SpielstandsManagerTest {
-
 	private SpielstandsManager spielstandsManager;
+
+
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -33,11 +34,62 @@ class SpielstandsManagerTest {
 		assertNotNull(spielstandsManager);
 	}
 
+	/**
+	 * Test method for {@link main.java.SpielstandsManager#setSpielstand(int, int)}.
+	 */
 	@Test
 	void testsetSpielstand() {
 		spielstandsManager.setSpielstand(3, 2);
 		assertEquals(3, spielstandsManager.getToreHeim());
 		assertEquals(2, spielstandsManager.getToreGast());
 	}
+
+	/**
+	 * Test method for {@link main.java.SpielstandsManager#resetSpielstand()}.
+	 */
+	@Test
+	void testResetSpielstand() {
+		spielstandsManager.setSpielstand(3, 2);
+		spielstandsManager.resetSpielstand();
+		assertEquals(0, spielstandsManager.getToreHeim());
+		assertEquals(0, spielstandsManager.getToreHeim());
+
+	}
+
+	/**
+	 * Test method for {@link main.java.SpielstandsManager#incToreHeim()}.
+	 */
+	@Test
+	void testIncToreHeim() {
+		spielstandsManager.setSpielstand(3, 2);
+		spielstandsManager.incToreHeim();
+		assertEquals(4, spielstandsManager.getToreHeim());
+	}
+
+	/**
+	 * Test method for {@link main.java.SpielstandsManager#incToreGast()}.
+	 */
+	@Test
+	void testIncToreGast() {
+		spielstandsManager.setSpielstand(3, 2);
+		spielstandsManager.incToreGast();
+		assertEquals(3, spielstandsManager.getToreGast());	}
+
+	/**
+	 * Test method for {@link main.java.SpielstandsManager#getToreHeim()}.
+	 */
+	@Test
+	void testGetToreHeim() {
+		spielstandsManager.setSpielstand(3, 2);
+		assertEquals(3, spielstandsManager.getToreHeim());
+	}
+
+	/**
+	 * Test method for {@link main.java.SpielstandsManager#getToreGast()}.
+	 */
+	@Test
+	void testGetToreGast() {
+		spielstandsManager.setSpielstand(3, 2);
+		assertEquals(2, spielstandsManager.getToreGast());	}
 
 }
